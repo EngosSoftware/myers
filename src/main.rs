f8e06336035813c8103a82aebef2795243ac4166
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-12-04 10:18:58 +0100

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use myers::{Modification, compare};
 
 /// Reads the input file.
 fn read_file(file_name: &str) -> Vec<String> {
-  let content = std::fs::read_to_string(file_name).expect("failed to read input file");
+  let content = std::fs::read_to_string(file_name).unwrap_or_else(|e| {
+    eprintln!("ERROR: Failed to read input file: {}\n{}", file_name, e);
+    std::process::exit(1);
+  });
   let mut buffer = vec![];
   for line in content.lines() {
     buffer.push(line.to_string());
@@ -12,15 +15,21 @@ fn read_file(file_name: &str) -> Vec<String> {
 
 /// Prints the edit script to standard output.
 fn print_report(file1: &[String], file2: &[String], modifications: &[Modification]) {
-  let _ = file1;
+  let (_, _) = (file1, file2);
+  println!("WORK IN PROGRESS; the following report will be better ;-)");
   for modification in modifications {
-    println!("+{} {:?}", file2[modification.line2 - 1], modification);
+    println!("{:?}", modification);
   }
 }
 
 fn main() {
-  let file1 = read_file("file1.txt");
-  let file2 = read_file("file2.txt");
-  let modifications = compare(&file1, &file2);
-  print_report(&file1, &file2, &modifications);
+  let args: Vec<String> = std::env::args().collect();
+  if args.len() == 3 {
+    let file1 = read_file(&args[1]);
+    let file2 = read_file(&args[2]);
+    let modifications = compare(&file1, &file2);
+    print_report(&file1, &file2, &modifications);
+  } else {
+    eprintln!("WORK IN PROGRESS");
+  }
 }
