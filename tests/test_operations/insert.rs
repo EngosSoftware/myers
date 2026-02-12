7779b3fd0f2ae4eb975fa69ca0b9a24f47971965
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-02-12 11:32:05 +0100

    Added tests.

--- tests/test_operations/insert.rs
+++ tests/test_operations/insert.rs
@@ -20,11 +20,6 @@ fn _0001() {
 fn _0002() {
   let a = v(&["a", "b", "d", "e"]);
   let b = v(&["a", "b", "c", "d", "e"]);
-  let modifications = compare(&a, &b);
-  assert_eq!(1, modifications.len());
-  assert_eq!(Op::Insert, modifications[0].op);
-  assert_eq!(2, modifications[0].line_1);
-  assert_eq!(3, modifications[0].line_2);
   assert_eq!(
     n(r#"
  1 1  a
@@ -38,12 +33,17 @@ fn _0002() {
 }
 
 #[test]
-fn single_insert_at_the_end() {
-  let a: Vec<String> = ["a", "b", "c", "d"].map(String::from).to_vec();
-  let b: Vec<String> = ["a", "b", "c", "d", "e"].map(String::from).to_vec();
-  let modifications = compare(&a, &b);
-  assert_eq!(1, modifications.len());
-  assert_eq!(Op::Insert, modifications[0].op);
-  assert_eq!(4, modifications[0].line_1);
-  assert_eq!(5, modifications[0].line_2);
+fn _0003() {
+  let a = v(&["a", "b", "c", "d"]);
+  let b = v(&["a", "b", "c", "d", "e"]);
+  assert_eq!(
+    n(r#"
+ 1 1  a
+ 2 2  b
+ 3 3  c
+ 4 4  d
+   5 +e
+"#),
+    report(&a, &b, &compare(&a, &b))
+  )
 }
